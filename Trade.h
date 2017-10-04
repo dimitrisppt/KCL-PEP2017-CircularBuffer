9e7d5a554ddb232b6a69f1a377acd35e45a57beb
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2017-10-04 12:33:37 +0100

    Implemented Trade

--- Trade.h
+++ Trade.h
@@ -3,7 +3,10 @@
 
 
 #include "vector.h"
+#include <iostream>
+using std::cout;
 using pep::vector;
+using std::endl;
 
 
 class Trade {
@@ -28,17 +31,33 @@ public:
 
 // TODO your code goes here:
 
+Trade bestBuySellTime(const vector<int> & prices) {
+	
+	
 
+    int buyTime = 0;
+    int sellTime = 0;
+ 
+	int diff = 0;
+	
+  	for (int i=0; i<prices.size()-1; i++) {
 
+		for (int j=i+1; j<prices.size(); j++)  {
+		  
+			if (prices[j] - prices[i] > diff) {
+				diff = prices[j] - prices[i];
+				buyTime = i;
+				sellTime = j;
+			}
+		}	  
+	}
+		  
 
-
-
-
-
-
-
-
-
+    Trade trade(buyTime, sellTime);
+	
+    return trade;
+	
+}
 
 // Don't write any code below this line
 
